8f87f65f6d69ba72f7d2ea8b61a850973a9aeb3f
amollberg <amollberg@users.noreply.github.com>
2020-02-01 11:09:49 +0100

    WIP letters

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+LETTERS = [
+  "BTFIHSKRHNKV",
+  "ZIVNJEIMAGFE",
+  "DSWGCVBYFCGR",
+  "RYRHAQDFAUDT",
+  "ZMXIESRPZTRI",
+  "QMSRSDOYEYQC",
+  "REOSASRZEMCA",
+  "GTAKRFRINMAL",
+  "WRJNBAICINMS",
+  "LYKPEXMDLPLO",
+  "RSQZOGGWTWSR",
+  "PXILQACENTRE",
+]
+
+class State():
+  def __init__(self):
+    self.players = [] #Initial positions
+
+def main():
+  print("Hello")
+
+if __name__ == '__main__':
+  main()
